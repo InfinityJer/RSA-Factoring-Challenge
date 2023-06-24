91269b49f115cb84df0c579344ad31408c77eed7
InfinityJer <jclaris6@gmail.com>
2023-06-24 04:37:27 -0700

    updates

--- factors.c
+++ factors.c
@@ -1,33 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "factor.h"
 
 /**
  * main - main function
  *
  *
- * Return: void
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+    FILE *fptr;
+    size_t count = 0;
+    char *buffer = NULL;
+    ssize_t line;
 
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: factor <filename>\n");
+        return 1;
+    }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: factor <filename>\n");
-		exit(EXIT_FAILURE);
-	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
-	{
-		factorize(buffer);
-	}
-return (0);
+    fptr = fopen(argv[1], "r");
+    if (fptr == NULL)
+    {
+        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+        return 1;
+    }
+
+    while ((line = getline(&buffer, &count, fptr)) != -1)
+    {
+        factorize(buffer);
+    }
+
+    free(buffer);
+    fclose(fptr);
+
+    return 0;
 }
